97723c90b240a36a08f045bfc81af2b177b8c46f
bartek szurgot <bartek.szurgot@baszerr.eu>
2012-08-10 08:30:50 +0200

    strict weak ordering of IP and Address

--- commons/src/Net/Address.hpp
+++ commons/src/Net/Address.hpp
@@ -1,7 +1,9 @@
 #ifndef INCLUDE_NET_ADDRESS_HPP_FILE
 #define INCLUDE_NET_ADDRESS_HPP_FILE
 
-#include <inttypes.h>
+#include <cinttypes>
+
+#include "Util/tabSize.hpp"
 
 
 namespace Net
@@ -39,6 +41,34 @@ S&& operator<<(S&& strm, const Address& addr)
 {
   strm << addr.ip_ << ':' << static_cast<int>(addr.port_);
   return strm;
+}
+
+
+inline bool operator<(const IPv4& lhs, const IPv4& rhs)
+{
+  for(size_t i=0; i<Util::tabSize(lhs.addr_); ++i)
+  {
+    if( lhs.addr_[i] < rhs.addr_[i] )
+      return true;
+    if( lhs.addr_[i] == rhs.addr_[i] )
+      continue;
+    return false;
+  }
+  // all elements are equal
+  return false;
+}
+
+
+inline bool operator<(const Address& lhs, const Address& rhs)
+{
+  // ip is enough?
+  if( lhs.ip_<rhs.ip_ )
+    return true;
+  // IP is not equal?
+  if( rhs.ip_<lhs.ip_ )
+    return false;
+  // if IP is equal, compare by ports.
+  return lhs.port_<rhs.port_;
 }
 
 }
--- commons/src/Net/Address.t.cpp
+++ commons/src/Net/Address.t.cpp
@@ -43,4 +43,59 @@ void testObj::test<2>(void)
   ensure_equals("invalid stream output", ss_.str(), "123.22.6.9:1234");
 }
 
+// test SWO for IPv4
+template<>
+template<>
+void testObj::test<3>(void)
+{
+  const IPv4 ip1={{1, 2, 3, 4}};
+
+  const IPv4 ip2={{2, 2, 3, 4}};
+  const IPv4 ip3={{1, 3, 3, 4}};
+  const IPv4 ip4={{1, 2, 4, 4}};
+  const IPv4 ip5={{1, 2, 3, 5}};
+
+  // test positive case
+  ensure("relation 1 fails", ip1<ip2);
+  ensure("relation 2 fails", ip1<ip3);
+  ensure("relation 3 fails", ip1<ip4);
+  ensure("relation 4 fails", ip1<ip5);
+
+  // test negative case
+  ensure("relation 5 fails", !(ip2<ip1));
+  ensure("relation 6 fails", !(ip3<ip1));
+  ensure("relation 7 fails", !(ip4<ip1));
+  ensure("relation 8 fails", !(ip5<ip1));
+
+  // test equality
+  ensure("relation 9 fails", !(ip1<ip1));
+}
+
+// test SWO for Address
+template<>
+template<>
+void testObj::test<4>(void)
+{
+  const Address a1 = {{{1,2,3,4}}, 400};
+  const Address a2 = {{{1,2,3,4}}, 500};
+  const Address a3 = {{{1,2,3,4}}, 300};
+  const Address a4 = {{{6,2,3,4}}, 400};
+  const Address a5 = {{{0,2,3,4}}, 400};
+
+  // test positive cases
+  ensure("relateion 1 fails", a1<a2);
+  ensure("relateion 2 fails", a1<a4);
+  ensure("relateion 3 fails", a3<a1);
+  ensure("relateion 4 fails", a5<a1);
+
+  // negative case
+  ensure("relateion 5 fails", !(a2<a1));
+  ensure("relateion 6 fails", !(a4<a1));
+  ensure("relateion 7 fails", !(a1<a3));
+  ensure("relateion 8 fails", !(a1<a5));
+
+  // equality
+  ensure("relation 9 failes", !(a1<a1));
+}
+
 } // namespace tut
