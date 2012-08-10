f03b5227d86bb97d8f19a058afaf5c48ee7205b0
bartek szurgot <bartek.szurgot@baszerr.eu>
2012-08-10 19:16:15 +0200

    Revert "removed test code from main()"
    This reverts commit d9d04dc184c329a6ad1e851db649008c8c90d693.

--- motherboard/uChaos/src/main.cpp
+++ motherboard/uChaos/src/main.cpp
@@ -1,4 +1,34 @@
+#include <string>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+#include "Crypto/Aes256.hpp"
+
+using namespace std;
+
+Crypto::BinData makeKIV(uint8_t mod=0)
+{
+  Crypto::BinData tmp( Crypto::Aes256::keySize() );
+  for(size_t i=0; i<tmp.size(); ++i)
+    tmp.data()[i]=i+mod;
+  return std::move(tmp);
+}
+
+
 int main(int argc, char **argv)
 {
+  std::cout << argv[0] << ": one big TODO... :)" << std::endl;
+  Crypto::Aes256 enc( makeKIV(66) );
+  Crypto::Aes256 dec( makeKIV(66) );
+  Crypto::Aes256::Data data;
+  for(const char *it="ala ma kota"; *it!=0; ++it)
+    data.push_back(*it);
+
+  enc.encrypt(data);
+  dec.decrypt(data);
+  cout << "got : |";
+  copy( data.begin(), data.end(), ostreambuf_iterator<char>(cout) );
+  cout << "|\n";
   return 0;
 }
