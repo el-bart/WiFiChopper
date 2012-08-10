cb2a48ad1ed839fa718cd99f36149197f56f9283
bartek szurgot <bartek.szurgot@baszerr.eu>
2012-08-10 16:42:07 +0200

    line communication, base on the abstractions, implemented

---
+++ commons/src/IO/LineComm.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <algorithm>
+
+#include "IO/LineComm.hpp"
+#include "Crypto/Aes256.hpp"
+#include "Net/TextLineChannel.hpp"
+#include "Util/hex.hpp"
+
+using namespace std;
+
+
+namespace IO
+{
+
+LineComm::Key::Key(void):
+  Crypto::BinData( Crypto::Aes256::keySize() )
+{ }
+
+
+LineComm::LineComm(Net::Channel chn, Crypto::BinData key):
+  crypt_( new Crypto::Aes256( std::move(key) ) ),
+  comm_( new Net::TextLineChannel( std::move(chn) ) )
+{ }
+
+
+void LineComm::sendLine(const std::string& line)
+{
+  // encrypt input data
+  Crypto::Algo::Data data( line.begin(), line.end() );
+  crypt_->encrypt(data);
+  // write encoded text as string
+  string encStr = Util::toHex(data);
+  // send encoded line
+  comm_->send( std::move(encStr) );
+}
+
+
+std::string LineComm::readLine(const double timeout)
+{
+  // read encrypted message
+  string encStr = comm_->read(timeout);
+  // conver it back to binary
+  Crypto::Algo::Data enc = Util::fromHex( std::move(encStr) );
+  // decode text
+  crypt_->decrypt(enc);
+  // return final message
+  return string( enc.begin(), enc.end() );
+}
+
+}
---
+++ commons/src/IO/LineComm.hpp
@@ -0,0 +1,36 @@
+#ifndef INCLUDE_IO_LINECOMM_HPP_FILE
+#define INCLUDE_IO_LINECOMM_HPP_FILE
+
+#include <string>
+#include <memory>
+
+#include "IO/TextLine.hpp"
+#include "Net/Channel.hpp"
+#include "Crypto/Algo.hpp"
+
+
+namespace IO
+{
+
+class LineComm
+{
+public:
+  struct Key: public Crypto::BinData
+  {
+    Key(void);
+  };
+
+
+  LineComm(Net::Channel chn, Crypto::BinData key);
+
+  void sendLine(const std::string& line);
+  std::string readLine(const double timeout);
+
+private:
+  std::unique_ptr<Crypto::Algo> crypt_;
+  std::unique_ptr<IO::TextLine> comm_;
+};
+
+}
+
+#endif
