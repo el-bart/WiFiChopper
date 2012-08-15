f469fd27316bc03f1d2c6410f2ce35e77a773264
bartek szurgot <bartek.szurgot@baszerr.eu>
2012-08-15 17:47:14 +0200

    added privileges dropping uppon start to motherboard-side deamon

--- motherboard/uChaos/src/main.cpp
+++ motherboard/uChaos/src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <stdexcept>
+#include <cstring>
+#include <cerrno>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
+#include "Util/Exception.hpp"
 #include "IO/LineCommUart.hpp"
 #include "IO/LineCommNetwork.hpp"
 #include "IO/ServerBuilder.hpp"
@@ -8,6 +14,25 @@
 #include "Crypto/readKeyFromFile.hpp"
 
 using namespace std;
+
+
+void dropPrivileges(const char* procName, const char* uidStr, const char* gidStr)
+{
+  assert( procName != nullptr );
+  assert( uidStr   != nullptr );
+  assert( gidStr   != nullptr );
+  // parse input
+  const uid_t uid = atoi(uidStr);
+  const gid_t gid = atoi(gidStr);
+  cout << procName << ": dropping privilages to UID=" << uid << " and GID=" << gid << endl;
+  // apply new permissions
+  if( setuid(uid) != 0 )
+    throw Util::Exception( UTIL_LOCSTRM << "unable to set UID=" << uid << ": " << strerror(errno) );
+  if( setgid(gid) != 0 )
+    throw Util::Exception( UTIL_LOCSTRM << "unable to set GID=" << gid << ": " << strerror(errno) );
+  // all done
+  cout << procName << ": now running as UID=" << getuid() << " and GID=" << getgid() << endl;
+}
 
 
 void handleClient(const char* procName, IO::LineComm& remote, IO::LineComm& dev)
@@ -35,9 +60,9 @@ void handleClient(const char* procName, IO::LineComm& remote, IO::LineComm& dev)
 
 int main(int argc, char **argv)
 {
-  if( argc!=1+2+1+1 )
+  if( argc!=1+2+1+1+2 )
   {
-    cerr << argv[0] << " <host> <port> <key_file> <dev>" << endl;
+    cerr << argv[0] << " <host> <port> <key_file> <dev> <uid> <gid>" << endl;
     return 1;
   }
 
@@ -46,6 +71,7 @@ int main(int argc, char **argv)
     cout << argv[0] << ": uChaos server is initializing..." << endl;
     IO::BuilderPtr  builder( new IO::ServerBuilder( Net::parseAddress(argv[1], argv[2]), Crypto::readKeyFromFile(argv[3]) ) );
     IO::LineCommPtr dev( new IO::LineCommUart(argv[4]) );
+    dropPrivileges(argv[0], argv[5], argv[6]);
 
     // main client-processing loop
     while(true)
