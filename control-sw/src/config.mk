b1d2d1f02dd34ab0d87d23b2e53fd732f62b1cc3
bartek szurgot <bartek.szurgot@baszerr.eu>
2012-08-24 11:37:12 +0200

    updated build process for small porjects to the v2.3.0

--- control-sw/src/config.mk
+++ control-sw/src/config.mk
@@ -1,11 +1,37 @@
 # name of the application
 export PROGRAM_NAME:=control-sw
 
+# default toolchain - can be changed with 'make TC=xxx'
+export TC:=gcc
+
 # toolchain
+ifeq ($(TC),gcc)
 export CC :=gcc
 export CXX:=g++
 export LD :=$(CXX)
+else
+ifeq ($(TC),avr)
+# tools
+export CC :=avr-gcc
+export CXX:=avr-g++
+export OBJCOPY:=avr-objcopy
+export LD :=$(CC)
+# tools settings
+export LFUSE:=255
+export HFUSE:=255
+# type of MCU for the avrdude programmer
+export MCU_PROG_TYPE:=m8
+# mype of MCU for the compiler
+export MCU_TYPE:=atmega8
+export CFLAGS  +=-mmcu=$(MCU_TYPE)
+export CXXFLAGS+=-mmcu=$(MCU_TYPE)
+export LDFLAGS +=-mmcu=$(MCU_TYPE)
+else
+$(error UNKNWON TOOLCHAIN '$(TC)'; USE 'gcc' (i.e. local) or 'avr')
+endif # TC=avr
+endif # TC=gcc
 
+# av lib
 LIBAV_FLAGS:=$(shell pkg-config libavutil --cflags)
 LIBAV_LIBS :=$(shell pkg-config libavutil --libs)
 
@@ -13,3 +39,6 @@ LIBAV_LIBS :=$(shell pkg-config libavutil --libs)
 export CFLAGS  +=
 export CXXFLAGS+=-std=c++11 $(LIBAV_FLAGS)
 export LDFLAGS +=-lopencv_highgui -lopencv_core -lopencv_imgproc -lmcrypt $(LIBAV_LIBS) -lrt
+
+# default target
+export DEFAULT_TARGET:=release
--- rt-controller/tests/ut/config.mk
+++ rt-controller/tests/ut/config.mk
@@ -1,12 +1,40 @@
 # name of the application
-export PROGRAM_NAME:=test_program
+export PROGRAM_NAME:=unit_tests
+
+# default toolchain - can be changed with 'make TC=xxx'
+export TC:=gcc
 
 # toolchain
+ifeq ($(TC),gcc)
 export CC :=gcc
 export CXX:=g++
 export LD :=$(CXX)
+else
+ifeq ($(TC),avr)
+# tools
+export CC :=avr-gcc
+export CXX:=avr-g++
+export OBJCOPY:=avr-objcopy
+export LD :=$(CC)
+# tools settings
+export LFUSE:=255
+export HFUSE:=255
+# type of MCU for the avrdude programmer
+export MCU_PROG_TYPE:=m8
+# mype of MCU for the compiler
+export MCU_TYPE:=atmega8
+export CFLAGS  +=-mmcu=$(MCU_TYPE)
+export CXXFLAGS+=-mmcu=$(MCU_TYPE)
+export LDFLAGS +=-mmcu=$(MCU_TYPE)
+else
+$(error UNKNWON TOOLCHAIN '$(TC)'; USE 'gcc' (i.e. local) or 'avr')
+endif # TC=avr
+endif # TC=gcc
 
 # extra flags, if needed
 export CFLAGS  +=
 export CXXFLAGS+=-std=c++11
 export LDFLAGS +=
+
+# default target
+export DEFAULT_TARGET:=test
